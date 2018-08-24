5272dff13109d3ee69ac5f28e0e8e821b5d3c8ce
jvaibhav123 <jvaibhav123@gmail.com>
2018-08-24 11:14:56 +0000

    Update hello.py

--- app/hello.py
+++ app/hello.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import random
-#import pymysql
+import pymysql
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
